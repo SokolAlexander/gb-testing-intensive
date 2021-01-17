07014d17c6d714b982b50ba550b1edde149767a5
SokolAlexander <asokol1994@gmail.com>
2021-01-17 09:42:13 +0300

    Practice-2: tests removed

--- cypress/integration/main_page.js
+++ cypress/integration/main_page.js
@@ -1,9 +1,5 @@
-describe("Main Page tests", { defaultCommandTimeout: 7000 }, () => {
-  beforeEach(() => {});
-  // it("Loads page and article", () => {
-  //   cy.visit('');
-  //   cy.get(".post");
-  // });
+describe("Main Page tests", () => {
+  // beforeEach(() => {});
 
   it("gets correct response", () => {
     cy.intercept("GET", "https://spaceflightnewsapi.net/api/v2/articles/", {
@@ -25,23 +21,5 @@ describe("Main Page tests", { defaultCommandTimeout: 7000 }, () => {
       expect(req.response.statusCode).to.equal(500);
     });
     cy.get('.error');
-  });
-
-  it('fails always', () => {
-    expect(2).to.equal(1);
-  })
-
-  it('shows a link to source', () => {
-    cy.intercept("GET", "https://spaceflightnewsapi.net/api/v2/articles/", {
-      headers: { "Access-Control-Allow-Origin": "*" },
-      fixture: "articles.json",
-    }).as("articlesRequest");
-    cy.visit("");
-    cy.get('.post').first().invoke('attr', 'href')
-      .then(href => {
-        cy.fixture("articles.json").then(articles => {
-          expect(href).to.equal(articles[0].url);
-      });
-    });
   });
 });
